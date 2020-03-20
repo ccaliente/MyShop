3aee51c55666619a8bea62127e73b7a9ee609056
Caliente <mariq.grafova@gmail.com>
2020-03-20 15:39:15 +0200

    Added Basket Model

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -17,5 +18,7 @@ namespace MyShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
