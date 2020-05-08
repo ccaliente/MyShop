b18a52125e902c8ce0f2c4bace2e3f11c8e92ca6
Caliente <mariq.grafova@gmail.com>
2020-05-08 13:35:15 +0300

    CRUD Products using ajax/jquery

--- MyShop/MyShop.UI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.UI/Controllers/ProductManagerController.cs
@@ -24,21 +24,70 @@ namespace MyShop.UI.Controllers
         // GET: ProductManager
         public ActionResult Index()
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
-        }
-
-        public ActionResult Create()
-        {
             ProductManagerViewModel viewModel = new ProductManagerViewModel();
-
             viewModel.Product = new Product();
             viewModel.ProductCategories = productCategories.Collection();
+
+            //List<Product> products = context.Collection().ToList();
+            //return View(products);
             return View(viewModel);
         }
 
+        public ActionResult GetData()
+        {
+            List<Product> data = context.Collection().ToList();
+            return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+        }
+
+        //[HttpPost]
+        //public ActionResult AddOrEditPartial(Product pr, HttpPostedFileBase file)
+        //{
+        //    bool status = false;
+        //    if (ModelState.IsValid)
+        //    {
+
+        //        var data = context.Find(pr.Id);
+        //        if (data != null)
+        //        {
+        //            data.Category = pr.Category;
+        //            context.Commit();
+        //        }
+        //        else
+        //        {
+        //            if (file != null)
+        //            {
+        //                pr.Image = pr.Id + Path.GetExtension(file.FileName); //запазваме името на файла, който качваме като ид+иметона файла
+        //                file.SaveAs(Server.MapPath("//Content//ProductImages//") + pr.Image); //запазваме файла на физическия път 
+        //            }
+        //            context.Insert(pr);
+        //            context.Commit();
+        //        }
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    return new JsonResult { Data = new { status = status } };
+
+        //}
+
+        [HttpGet]
+        public ActionResult AddOrEditPartial(string Id)
+        {
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+            //viewModel.Product = new Product();
+            //viewModel.ProductCategories = productCategories.Collection();
+            //return View(viewModel);
+
+            viewModel.Product = new Product();
+            viewModel.Product = context.Find(Id);
+            viewModel.ProductCategories = productCategories.Collection();
+            return PartialView("AddOrEditPartial", viewModel);
+            //return View("AddOrEditPartial", viewModel);
+
+        }
+
         [HttpPost]
-        public ActionResult Create(Product product, HttpPostedFileBase file)
+        public ActionResult AddOrEditPartial(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -46,35 +95,83 @@ namespace MyShop.UI.Controllers
             }
             else
             {
-                if (file != null)
+                var data = context.Find(product.Id);
+                if (data != null)
                 {
-                    product.Image = product.Id + Path.GetExtension(file.FileName); //запазваме името на файла, който качваме като ид+иметона файла
-                    file.SaveAs(Server.MapPath("//Content//ProductImages//")+product.Image); //запазваме файла на физическия път 
+                    if (file != null)
+                    {
+                        product.Image = product.Id + Path.GetExtension(file.FileName); //запазваме името на файла, който качваме като ид+иметона файла
+                        file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image); //запазваме файла на физическия път 
+                    }
+                    product.Category = product.Category;
+                    product.Description = product.Description;
+                    product.Name = product.Name;
+                    product.Price = product.Price;
                 }
-                context.Insert(product);
-                context.Commit();
+                else
+                {
+                    if (file != null)
+                    {
+                        product.Image = product.Id + Path.GetExtension(file.FileName); //запазваме името на файла, който качваме като ид+иметона файла
+                        file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image); //запазваме файла на физическия път 
+                    }
+                    context.Insert(product);
+                    context.Commit();
+                }
+               
 
                 return RedirectToAction("Index");
             }
 
         }
 
-        public ActionResult Edit(string Id)
-        {
-            Product product = context.Find(Id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            else
-            {
-                ProductManagerViewModel viewModel = new ProductManagerViewModel();
-                viewModel.Product = product;
-                viewModel.ProductCategories = productCategories.Collection();
+        //public ActionResult Create()
+        //{
+        //    ProductManagerViewModel viewModel = new ProductManagerViewModel();
 
-                return View(viewModel);
-            }
-        }
+        //    viewModel.Product = new Product();
+        //    viewModel.ProductCategories = productCategories.Collection();
+        //    return View(viewModel);
+        //}
+
+        //[HttpPost]
+        //public ActionResult Create(Product product, HttpPostedFileBase file)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(product);
+        //    }
+        //    else
+        //    {
+        //        if (file != null)
+        //        {
+        //            product.Image = product.Id + Path.GetExtension(file.FileName); //запазваме името на файла, който качваме като ид+иметона файла
+        //            file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image); //запазваме файла на физическия път 
+        //        }
+        //        context.Insert(product);
+        //        context.Commit();
+
+        //        return RedirectToAction("Index");
+        //    }
+
+        //}
+
+        //public ActionResult Edit(string Id)
+        //{
+        //    Product product = context.Find(Id);
+        //    if (product == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    else
+        //    {
+        //        ProductManagerViewModel viewModel = new ProductManagerViewModel();
+        //        viewModel.Product = product;
+        //        viewModel.ProductCategories = productCategories.Collection();
+
+        //        return View(viewModel);
+        //    }
+        //}
 
         [HttpPost]
         public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
@@ -107,37 +204,53 @@ namespace MyShop.UI.Controllers
             }
         }
 
-        public ActionResult Delete(string Id)
+        public JsonResult Delete(string Id)
         {
-            Product productToDelete = context.Find(Id);
+            Product prod = new Product();
+            prod = context.Find(Id);
+            if (prod != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    context.Delete(Id);
+                    context.Commit();
+                }
+            }
+            return Json("success", JsonRequestBehavior.AllowGet);
 
-            if (productToDelete == null)
-            {
-                return HttpNotFound();
-            }
-            else
-            {
-                return View(productToDelete);
-            }
         }
 
-        [HttpPost]
-        [ActionName("Delete")]
-        public ActionResult ConfirmDelete(string Id)
-        {
-            Product productToDelete = context.Find(Id);
+        //public ActionResult Delete(string Id)
+        //{
+        //    Product productToDelete = context.Find(Id);
 
-            if (productToDelete == null)
-            {
-                return HttpNotFound();
-            }
-            else
-            {
-                context.Delete(Id);
-                context.Commit();
-                return RedirectToAction("Index");
-            }
-        }
+        //    if (productToDelete == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    else
+        //    {
+        //        return View(productToDelete);
+        //    }
+        //}
+
+        //[HttpPost]
+        //[ActionName("Delete")]
+        //public ActionResult ConfirmDelete(string Id)
+        //{
+        //    Product productToDelete = context.Find(Id);
+
+        //    if (productToDelete == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    else
+        //    {
+        //        context.Delete(Id);
+        //        context.Commit();
+        //        return RedirectToAction("Index");
+        //    }
+        //}
 
     }
 }
